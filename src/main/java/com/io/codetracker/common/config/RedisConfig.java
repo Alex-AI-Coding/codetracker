a6a28159c36e80b0b2e3f3aa832a3fc0f4ff62b6
dexxf <Dexter101306@gmail.com>
2026-08-08 00:39:54 +0800

    feat: add Redis caching and apply it to UserRepository (#115)
    * feat: add data-redis and starter cache dependency in pom.xml
    
    * feat: setup cache and add default RedisCacheManager
    
    * refactor: change default redis host to redis-cache
    
    * feat: add redis-cache service in docker-compose.dev.yml
    
    * refactor: use Application class package name in RedisConfig instead of hardcoding
    
    * feat: add jackson mixin so User can be deserialized from redis cache
    
    * feat: add UserRepository caching and replace Optional with UserNotFoundException
    
    * feat: disable caching null values

---
+++ src/main/java/com/io/codetracker/common/config/RedisConfig.java
@@ -0,0 +1,48 @@
+package com.io.codetracker.common.config;
+
+import com.io.codetracker.Application;
+import com.io.codetracker.adapter.user.out.persistence.mapper.UserJacksonMixIn;
+import com.io.codetracker.domain.user.entity.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
+
+        GenericJacksonJsonRedisSerializer serializer =
+                GenericJacksonJsonRedisSerializer.builder()
+                        .customize(
+                                builder -> builder
+                                        .addMixIn(User.class, UserJacksonMixIn.class)
+
+                        )
+                        .enableDefaultTyping(
+                                BasicPolymorphicTypeValidator.builder()
+                                        .allowIfSubType(Application.class.getPackageName())
+                                        .build()
+                        )
+                        .build();
+
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(serializer)
+                );
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
+    }
+}
