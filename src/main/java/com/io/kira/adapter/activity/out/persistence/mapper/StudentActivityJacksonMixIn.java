3fa2532f83c33b916a7b4c9e5c9117567f6807d0
dexxf <Dexter101306@gmail.com>
2026-08-31 00:10:20 +0800

    fix: student activity deserialization. missing mixin (#138)

---
+++ src/main/java/com/io/kira/adapter/activity/out/persistence/mapper/StudentActivityJacksonMixIn.java
@@ -0,0 +1,23 @@
+package com.io.kira.adapter.activity.out.persistence.mapper;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.io.kira.domain.activity.entity.StudentActivity;
+import com.io.kira.domain.activity.valueObject.SubmissionStatus;
+
+import java.util.UUID;
+
+public abstract class StudentActivityJacksonMixIn {
+
+    @JsonCreator
+    static StudentActivity reconstitute(
+            UUID studentActivityId,
+            UUID activityId,
+            UUID userId,
+            SubmissionStatus submissionStatus,
+            String feedback,
+            Integer score,
+            String submittedCommitSha) {
+        throw new UnsupportedOperationException();
+    }
+
+}
--- src/main/java/com/io/kira/common/config/RedisConfig.java
+++ src/main/java/com/io/kira/common/config/RedisConfig.java
@@ -2,10 +2,12 @@ package com.io.kira.common.config;
 
 import com.io.kira.Application;
 import com.io.kira.adapter.activity.out.persistence.mapper.ActivityJacksonMixIn;
+import com.io.kira.adapter.activity.out.persistence.mapper.StudentActivityJacksonMixIn;
 import com.io.kira.adapter.announcement.out.persistence.mapper.AnnouncementJacksonMixIn;
 import com.io.kira.adapter.classroom.out.persistence.mapper.ClassroomStudentJacksonMixIn;
 import com.io.kira.adapter.user.out.persistence.mapper.UserJacksonMixIn;
 import com.io.kira.domain.activity.entity.Activity;
+import com.io.kira.domain.activity.entity.StudentActivity;
 import com.io.kira.domain.announcement.entity.Announcement;
 import com.io.kira.domain.classroom.entity.ClassroomStudent;
 import com.io.kira.domain.user.entity.User;
@@ -37,6 +39,7 @@ public class RedisConfig {
         mixIns.put(ClassroomStudent.class, ClassroomStudentJacksonMixIn.class);
         mixIns.put(Announcement.class, AnnouncementJacksonMixIn.class);
         mixIns.put(Activity.class, ActivityJacksonMixIn.class);
+        mixIns.put(StudentActivity.class, StudentActivityJacksonMixIn.class);
 
         return mixIns;
 
